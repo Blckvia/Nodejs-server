100fcf9527963caa1e6b68209b19df712ee82a9f
blckvia <a.vyatkinn@icloud.com>
2022-11-22 14:56:03 +0500

    payment method added

---
+++ config.js
@@ -0,0 +1,5 @@
+// const STRIPE_TEST_API_KEY = process.env.STRIPE_TEST_API_KEY;
+const MONGODB_URL = process.env.MONGODB_URI;
+
+// export { STRIPE_TEST_API_KEY, MONGODB_URL };
+module.exports = MONGODB_URL;
